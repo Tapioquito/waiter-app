5413b24b12297a9e4b1252417187cefa64270ce3
tapioquito <caioportoramos@gmail.com>
2023-12-09 23:31:39 -0300

    added entities modelling

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
 
-const port = 3001;
 
-app.listen(3001, ()=>{
-    console.log(`Estamos no Brasiiiiil. Rodando na porta ${port}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+    .then(()=>{
+        const app = express();
+        const port = 3001;
+
+        app.listen(port, ()=>{
+            console.log(`Estamos no Brasiiiiil. Rodando na porta ${port}`);
+        });
+
+        console.log('Conectado ao MongoDB');
+    }).catch(()=>console.log('erro ao conectar ao mongoDB'));
---
+++ api/src/router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+export const router = Router();
+
+//List categories
+//Create category
+
+//List products
+//Create product
+
+//Get products by category
+
+//List orders
+//Create Order
+//Change order status
+//Delete | Cancel order
